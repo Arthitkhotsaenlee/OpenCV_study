a53389c6a6e6e5907d7aefced30e88d8cd0ae52f
arthitkhotsaenlee <arthit@mindampltd.com>
2022-09-06 14:51:18 +0800

    stack function

--- chapter5.py
+++ chapter5.py
@@ -2,3 +2,17 @@
 Warp perspective "Bird eyes view"
 """
 import cv2
+import numpy as np
+
+width, height = 250,350
+img = cv2.imread("Resources/Photos/pokemon_card.jpg")
+pts1 = np.float32([[111,300],[287,188],[154,482],[352,440]])  # declare 4 conner point of interesting
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])  # The referent tranfrom points
+metrix = cv2.getPerspectiveTransform(pts1,pts2)
+
+imgoutput = cv2.warpPerspective(img,metrix,(width,height))
+
+
+cv2.imshow("Image",img)
+cv2.imshow("Output Image",imgoutput)
+cv2.waitKey(0)
